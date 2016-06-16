02096ec75048bf13e814ea23342ca20968c0867c
AndyHe <vpan@vip.qq.com>
2016-06-16 19:25:09 +0800

    CoderTimer

---
+++ AndyHeCode/AH.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AH.Framework;
+
+namespace AH.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int iteration = 100 * 1000;
+            StringBuilder sb = new StringBuilder();
+            CodeTimer.Time("StringBuilder", iteration, () => sb.Append("A"));
+
+            string str = string.Empty;
+            CodeTimer.Time("String Concat", iteration, () =>
+            {
+                str += "A";
+
+            });
+            Console.ReadKey();
+        }
+    }
+}
